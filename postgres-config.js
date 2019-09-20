eb202a47cabd674ed5823ecd63a273990f01dba8
Ayako <ayakosayama1987@gmail.com>
2019-09-20 07:57:40 -0700

    deploy branch

--- postgres-config.js
+++ postgres-config.js
@@ -1,15 +1,24 @@
 
 //npm package to connnect postgres with node
 const pg = require('pg')
+require('dotenv').config();
 
 //connecting to the database that we want to query
+
+// 1st way: using URI
+const connectionString = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.POSTGRES_URI;
 const client = new pg.Client({
-  user: "postgres",
-  password: "alijan",
-  host: "localhost",
-  port: 5432,
-  database: "ayako_class"
+  connectionString: connectionString,
 })
+
+// 2nd way: using object
+// const client = new pg.Client({
+//   user: "postgres",
+//   password: "alijan",
+//   host: "localhost",
+//   port: 5432,
+//   database: "ayako_class"
+// })
 
 // async function connect() {
 //   try {
